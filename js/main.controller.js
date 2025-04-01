d92b87ca28e38e7b3bf8fe135d9c8e88d1d2bde5
Valery Vishnevezky <valery020201@gmail.com>
2025-04-02 00:22:54 +0300

    basic app

---
+++ js/main.controller.js
@@ -0,0 +1,17 @@
+
+const sound = new Audio('/sound/happy-birthday-to-you-jazz-94100.mp3')
+
+function onStart() {
+	const elStart = document.querySelector('.start').classList.add('hide')
+	const elMain = document.querySelector('.main').classList.remove('hide')
+	startConfetti() 
+	sound.play()
+}
+
+function onPlay(elCheckbox) {
+    if (!elCheckbox.checked) {
+        sound.play()
+    } else {
+        sound.pause()
+    }
+}
---
+++ js/services/main.service.js
@@ -0,0 +1,64 @@
+
+function burstConfetti() {
+  confetti({
+	angle: randomInRange(55, 125),
+	spread: randomInRange(50, 70),
+	particleCount: randomInRange(50, 100),
+	origin: { y: 1 },
+  });
+}
+
+function startSideConfetti(duration = 1000) {
+	const end = Date.now() + duration;
+	const colors = ["#add8e6", "#ffb6c1"];
+  
+	function frame() {
+	  confetti({
+		particleCount: 2,
+		angle: 60,
+		spread: 55,
+		origin: { x: 0 },
+		colors: colors,
+	  });
+  
+	  confetti({
+		particleCount: 2,
+		angle: 120,
+		spread: 55,
+		origin: { x: 1 },
+		colors: colors,
+	  });
+  
+	  if (Date.now() < end) {
+		requestAnimationFrame(frame);
+	  }
+	}
+  
+	frame();
+  }
+
+
+function startConfetti(duration = 1500) {
+	const animationEnd = Date.now() + duration
+	const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 }
+	const interval = setInterval(function () {
+		const timeLeft = animationEnd - Date.now()
+		if (timeLeft <= 0) {
+			clearInterval(interval)
+			return
+		}
+		const particleCount = 50 * (timeLeft / duration)
+		confetti(
+			Object.assign({}, defaults, {
+				particleCount,
+				origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+			})
+		)
+		confetti(
+			Object.assign({}, defaults, {
+				particleCount,
+				origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+			})
+		)
+	}, 250)
+}
